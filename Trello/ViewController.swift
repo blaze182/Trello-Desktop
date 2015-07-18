f95cbed938d9dac41ae06ddfcfa961c066d55ee5
Paul Blaze <paul.ovechkin@gmail.com>
2015-07-18 20:55:20 +0300

    Initial Commit

---
+++ Trello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trello
+//
+//  Created by Pavel Ovechkin on 18/07/15.
+//  Copyright (c) 2015 orbitum. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
